3f3882d847dc45939136af2be07a31b21b8f9f15
Franco Minati <francominati@gmail.com>
2022-07-16 11:50:33 -0300

    Primeros colores elegidos

---
+++ scripts/clase1-p.js
@@ -0,0 +1,12 @@
+let num1 = prompt("Ingrese un numero")
+let num2 = prompt("Ingrese otro numero")
+
+let suma = Number(num1)+Number(num2)
+let resta= num1-num2
+let multi= num1*num2
+
+alert("La suma es: "+ suma)
+alert("La resta es: "+ resta)
+alert("La multiplicacion es: "+ multi)
+
+console.log(suma, resta, multi)
