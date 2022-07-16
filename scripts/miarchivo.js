f1305741f418d853476cd1c47020032a188c5050
Franco Minati <francominati@gmail.com>
2022-07-16 15:55:03 -0300

    Clase 2 y 3 JS

---
+++ scripts/miarchivo.js
@@ -0,0 +1,12 @@
+let numeroPedido=parseInt(prompt("Ingrese un numero"))
+if(numeroPedido>1000){
+    alert("El valor ingresado es mayor que 1000")
+}
+let textoIngresado=prompt("Ingrese texto").toUpperCase()
+if(textoIngresado=="HOLA"){
+    alert("IngresasteL: "+textoIngresado)
+}
+numeroPedido=prompt("Ingrese un numero")
+if(numeroPedido>=10 && numeroPedido<=50){
+    alert("El numero ingresado esta entre 10 y 50")
+}
